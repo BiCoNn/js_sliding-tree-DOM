ed5dbf2a80e4bf94a6fadb2d68184a74af089774
Nazar <nazar.shevchuk.121@gmail.com>
2025-08-21 19:57:14 +0300

    sol1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
+const style = document.createElement('style');
+
+style.textContent = `
+.active {
+display: none;}`;
+document.head.append(style);
+
 // write code here
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', function () {
+  const target = event.target.closest('LI');
+
+  if (target.childElementCount !== 0) {
+    const innertarger = target.querySelector('UL');
+
+    innertarger.classList.toggle('active');
+  }
+});
